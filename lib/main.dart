25025caba7054a1538f0cff62fd40a896ceeaa2c
kalachlegends <artem.kalachev.2002@gmail.com>
2023-07-05 18:00:40 +0600

    feat: dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'dart:developer';
 
 void main() {
   runApp(const CryptoList());
@@ -82,21 +85,44 @@ class _CryptoListScreenState extends State<CryptoListScreen> {
                   Icons.arrow_forward_ios,
                 ),
                 onTap: () => {
-                  Navigator.of(context).pushNamed('/coin', arguments: coinName)
+                  Navigator.of(context)
+                      .pushNamed('/coin', arguments: '$coinName $i')
                 },
               );
             }));
   }
 }
 
-class CryptoCoinScreen extends StatelessWidget {
+class CryptoCoinScreen extends StatefulWidget {
   const CryptoCoinScreen({super.key});
+
+  @override
+  State<CryptoCoinScreen> createState() => _CryptoCoinScreenState();
+}
+
+class _CryptoCoinScreenState extends State<CryptoCoinScreen> {
+  String? coinName;
+  @override
+  void didChangeDependencies() {
+    final args = ModalRoute.of(context)?.settings.arguments;
+    if (args == null) {
+      //  log('You must provide args');
+      return;
+    }
+    if (args is! String) {
+      // log('You must provide args');
+      return;
+    }
+    coinName = args;
+    setState(() {});
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Bitcoin"),
+        title: Text(coinName ?? '..'),
       ),
     );
   }
