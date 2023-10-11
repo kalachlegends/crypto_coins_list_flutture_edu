d55da3a5731d852222c03e0ae7a9dc70ef859ad3
kalachlegends <artem.kalachev.2002@gmail.com>
2023-10-11 08:33:31 +0600

    feat: firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,20 @@
-import 'dart:math';
-
-import 'package:crypto_coins_list/features/crypto_list/crypto_list.dart';
-import 'package:crypto_coins_list/repositories/crypto_coins/crypto_coins_repository.dart';
-import 'package:crypto_coins_list/router/router.dart';
-import 'package:crypto_coins_list/theme/theme.dart';
+import 'package:crypto_coins_list/firebase_options.dart';
 import 'package:dio/dio.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
-import 'dart:developer' as developer;
+
 import 'package:crypto_coins_list/repositories/crypto_coins/crypto_coins.dart';
 import 'crypto_coins_lis_app.dart';
-import 'features/crypto_coin/views/crypto_coin_screen.dart';
+// import 'package:talker_flutter/talker_flutter.dart';
 
-void main() {
+void main() async {
+  // final talker = TalkerFlutter.init();
+  // GetIt.I.registerSingleton(talker);
+  // GetIt.I<Talker>().debug('Talker started');
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   GetIt.I.registerLazySingleton<AbstractCoinsRepository>(
       () => CryptoCoinsRepository(dio: Dio()));
   runApp(const CryptoListApp());
